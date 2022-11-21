187d2d3f1ca4a31e281620db8dcb6e2acfd0b95a
mlimony <mlimony@gmail.com>
2022-11-22 00:13:40 +0200

    add webhook support

--- bot_config.py
+++ bot_config.py
@@ -4,3 +4,7 @@ from pydantic import SecretStr
 
 class BotConfig(BaseConfig):
     bot_token: SecretStr
+    use_webhook: bool = False
+    webhook_host: str = "0.0.0.0"
+    webhook_port: int = 3978
+    webhook_url: str
